a0263c712d372955530e7fb672fc971963659d26
cocobilbao <cocobilbao@gmail.com>
2018-11-05 17:46:09 +0100

    more things falling

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,154 +1,329 @@
-
-
 window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
 
-  function Canvas(id){
+  function Canvas(id) {
     this.canvas = document.getElementById(id);
     this.ctx = this.canvas.getContext("2d");
-    
-}
+  }
 
-function Background(canvas) {
-  this.canvas = canvas;
-  this.ctx = this.canvas.getContext("2d");
-  this.width = this.canvas.width;
-  this.height = this.canvas.height;
-  this.x = 0;
-  this.y = 0;
-
-  this.img = new Image();
-  this.img.src = "images/GracelandMansion.png";
-
-}
-
-Background.prototype.draw = function() {
-   
-  this.ctx.drawImage(this.img, this.x, this.y, this.canvas.width, this.canvas.height);
-     
-
-}
-
-
-function Player(canvas){
-  
-  this.canvas = canvas;
-  this.ctx = this.canvas.getContext("2d");
-  this.x = 450;
-  this.y = 430;
-  this.width; 
-  this.height;
-
-  this.vx = 10;
- 
-
-  
-
-  this.img = new Image();
-  this.img.src="images/elvis-left.png";
-
-
-  this.setListeners();
-}
-
-var KEY_RIGHT = 39;
-var KEY_LEFT = 37;
-
-
-
-
-
-Player.prototype.draw = function(){
-
-  this.ctx.drawImage(this.img, this.x, this.y, 130, 160)
-
-
-}
-
-Player.prototype.setListeners = function() {
-  document.onkeydown = function(event) {
-    if (event.keyCode === KEY_LEFT) {
-      this.x -= 10;
-      this.vx -= 10;
-    } else if (event.keyCode === KEY_RIGHT){
-      this.x += 10;
-      this.vx += 10;
-    }
-  }.bind(this);
-};
-
-Player.prototype.move=function(){
- 
-  if(this.x <= -5){
+  function Background(canvas) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
     this.x = 0;
-  } if (this.x >= 852){
-    this.x = 847;
+    this.y = 0;
+
+    this.img = new Image();
+    this.img.src = "images/GracelandMansion.png";
   }
- 
+
+  Background.prototype.draw = function() {
+    this.ctx.drawImage(
+      this.img,
+      this.x,
+      this.y,
+      this.canvas.width,
+      this.canvas.height
+    );
+  };
+
+  function Player(canvas) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.x = 450;
+    this.y = 430;
+    this.width;
+    this.height;
+
+    this.vx = 10;
+
+    this.img = new Image();
+    this.img.src = "images/elvis-left.png";
+
+    this.setListeners();
   }
 
+  var KEY_RIGHT = 39;
+  var KEY_LEFT = 37;
 
-function Things(canvas) {
+  Player.prototype.draw = function() {
+    this.ctx.drawImage(this.img, this.x, this.y, 130, 160);
+  };
 
+  Player.prototype.setListeners = function() {
+    document.onkeydown = function(event) {
+      if (event.keyCode === KEY_LEFT) {
+        this.x -= 10;
+        this.vx -= 10;
+      } else if (event.keyCode === KEY_RIGHT) {
+        this.x += 10;
+        this.vx += 10;
+      }
+    }.bind(this);
+  };
+
+  Player.prototype.move = function() {
+    if (this.x <= -5) {
+      this.x = 0;
+    }
+    if (this.x >= 852) {
+      this.x = 847;
+    }
+  };
+
+  // objetos que caen, principio.
+
+  //Disco
+
+  function Record(canvas) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.x = Math.floor(Math.random() * 955);
+    this.y = -40;
+    this.width;
+    this.height;
+
+    this.vy = 0.3;
+
+    this.img = new Image();
+    this.img.src = "images/disco.png";
+  }
+
+  Record.prototype.draw = function() {
+    this.ctx.drawImage(this.img, this.x, this.y, 80, 80);
+  };
+
+  Record.prototype.move = function() {
+    if (this.y > 700) {
+      this.y = -40;
+      this.x = Math.floor(Math.random() * 955);
+    }
+
+    this.y += this.vy;
+  };
+//Tarta
+
+  function Cake(canvas) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.x = Math.floor(Math.random() * 955);
+    this.y = -80;
+    this.width;
+    this.height;
+
+    this.vy = 0.5;
+
+    this.img = new Image();
+    this.img.src = "images/cheesecake.png";
+  }
+
+  Cake.prototype.draw = function() {
+    this.ctx.drawImage(this.img, this.x, this.y, 60, 60);
+  };
+
+  Cake.prototype.move = function() {
+    if (this.y > 700) {
+      this.y = -80;
+      this.x = Math.floor(Math.random() * 955);
+    }
+
+    this.y += this.vy;
+  };
+
+// Pirula
+
+  function Pil(canvas) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.x = Math.floor(Math.random() * 955);
+    this.y = -160;
+    this.width;
+    this.height;
+
+    this.vy = 0.4;
+
+    this.img = new Image();
+    this.img.src = "images/pil.png";
+  }
+
+  Pil.prototype.draw = function() {
+    this.ctx.drawImage(this.img, this.x, this.y, 60, 60);
+  };
+
+  Pil.prototype.move = function() {
+    if (this.y > 700) {
+      this.y = -180;
+      this.x = Math.floor(Math.random() * 955);
+    }
+
+    this.y += this.vy;
+  };
+
+  // Hamburguesa
+
+  function Burger(canvas) {
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.x = Math.floor(Math.random() * 955);
+    this.y = -160;
+    this.width;
+    this.height;
+
+    this.vy = 0.6;
+
+    this.img = new Image();
+    this.img.src = "images/burger.png";
+  }
+
+  Burger.prototype.draw = function() {
+    this.ctx.drawImage(this.img, this.x, this.y, 70, 50);
+  };
+
+  Burger.prototype.move = function() {
+    if (this.y > 700) {
+      this.y = -180;
+      this.x = Math.floor(Math.random() * 955);
+    }
+
+    this.y += this.vy;
+  };
+
+//Tarta
+
+function Cake2(canvas) {
   this.canvas = canvas;
   this.ctx = this.canvas.getContext("2d");
-  this.x = Math.floor(Math.random()*975);
-  this.y = -20;
-  this.width; 
+  this.x = Math.floor(Math.random() * 955);
+  this.y = -280;
+  this.width;
   this.height;
 
   this.vy = 0.4;
 
   this.img = new Image();
-  this.img.src="images/disco.png";
-
+  this.img.src = "images/cheesecake2.png";
 }
 
-Things.prototype.draw = function(){
+Cake2.prototype.draw = function() {
+  this.ctx.drawImage(this.img, this.x, this.y, 60, 60);
+  this.ctx.rotate(-this.angle + Math.PI/2.0);
+};
 
-  this.ctx.drawImage(this.img, this.x, this.y, 80, 80)
-
-}
-
- 
-Things.prototype.move= function(){
-  if(this.y > 700){
-    this.y = -20;
-    this.x = Math.floor(Math.random()*975);
+Cake2.prototype.move = function() {
+  if (this.y > 700) {
+    this.y = -480;
+    this.x = Math.floor(Math.random() * 955);
   }
 
   this.y += this.vy;
+};
 
-    }
+// Pirula
 
-  
+function Pil2(canvas) {
+  this.canvas = canvas;
+  this.ctx = this.canvas.getContext("2d");
+  this.x = Math.floor(Math.random() * 955);
+  this.y = -160;
+  this.width;
+  this.height;
+
+  this.vy = 0.5;
+
+  this.img = new Image();
+  this.img.src = "images/pil2.png";
+}
+
+Pil2.prototype.draw = function() {
+  this.ctx.drawImage(this.img, this.x, this.y, 60, 60);
+};
+
+Pil2.prototype.move = function() {
+  if (this.y > 700) {
+    this.y = -580;
+    this.x = Math.floor(Math.random() * 955);
+  }
+
+  this.y += this.vy;
+};
+
+// Hamburguesa
+
+function Burger2(canvas) {
+  this.canvas = canvas;
+  this.ctx = this.canvas.getContext("2d");
+  this.x = Math.floor(Math.random() * 955);
+  this.y = -160;
+  this.width;
+  this.height;
+
+  this.vy = 0.4;
+
+  this.img = new Image();
+  this.img.src = "images/burger.png";
+}
+
+Burger2.prototype.draw = function() {
+  this.ctx.drawImage(this.img, this.x, this.y, 70, 50);
+};
+
+Burger2.prototype.move = function() {
+  if (this.y > 700) {
+    this.y = -180;
+    this.x = Math.floor(Math.random() * 955);
+  }
+
+  this.y += this.vy;
+};
+
+
+
+  // Objetos que caen, final.
 
   var canvas = document.getElementById("game");
   var ctx = canvas.getContext("2d");
   var background = new Background(canvas);
   var player = new Player(canvas);
-  var things = new Things(canvas);
-
-
-
-
+  var record = new Record(canvas);
+  var cake = new Cake(canvas);
+  var pil = new Pil(canvas);
+  var burger = new Burger(canvas);
+  var cake2 = new Cake2(canvas);
+  var pil2 = new Pil2(canvas);
+  var burger2 = new Burger2(canvas);
 
   function startGame() {
-    setInterval (function(){
-
+    setInterval(function() {
       background.draw();
 
       player.draw();
       player.move();
 
-      things.draw();
-      things.move();
-      
+      record.draw();
+      record.move();
 
+      cake.draw();
+      cake.move();
 
-    } ), 1000/60  }
+      pil.draw();
+      pil.move();
 
-  
+      burger.draw();
+      burger.move();
+
+      cake2.draw();
+      cake2.move();
+
+      pil2.draw();
+      pil2.move();
+
+      burger2.draw();
+      burger2.move();
+
+    }),
+      1000 / 60;
+  }
 };

1c4a7076e9266967787dea98e3f4e742b6904e00
cocobilbao <cocobilbao@gmail.com>
2018-11-05 13:22:04 +0100

    falling thing add

--- javascripts/script.js
+++ javascripts/script.js
@@ -8,6 +8,7 @@ window.onload = function() {
   function Canvas(id){
     this.canvas = document.getElementById(id);
     this.ctx = this.canvas.getContext("2d");
+    
 }
 
 function Background(canvas) {
@@ -43,21 +44,28 @@ function Player(canvas){
   this.vx = 3;
   this.vy = 3;
 
+  
+
   this.img = new Image();
   this.img.src="images/elvis-left.png";
+
+
   this.setListeners();
 }
 
 var KEY_RIGHT = 39;
 var KEY_LEFT = 37;
 
+
+
+
+
 Player.prototype.draw = function(){
 
   this.ctx.drawImage(this.img, this.x, this.y, 130, 160)
 
+
 }
-
-
 
 Player.prototype.setListeners = function() {
   document.onkeydown = function(event) {
@@ -73,17 +81,51 @@ Player.prototype.setListeners = function() {
 
 Player.prototype.move=function(){
  
-  if(this.x <= 0){
-    this.x = 10;
-  } if (this.x >= 590 ){
-    this.x = 580;
+  if(this.x <= -5){
+    this.x = 0;
+  } if (this.x >= 852){
+    this.x = 847;
   }
+ 
   }
+
+
+function Things(canvas) {
+
+  this.canvas = canvas;
+  this.ctx = this.canvas.getContext("2d");
+  this.x = 500;
+  this.y = 0;
+  this.width; 
+  this.height;
+
+  this.vy = 0.5;
+
+  this.img = new Image();
+  this.img.src="images/disco.png";
+
+}
+
+Things.prototype.draw = function(){
+
+  this.ctx.drawImage(this.img, this.x, this.y, 80, 80)
+
+}
+
+ 
+Things.prototype.move= function(){
+ 
+  this.y += this.vy;
+
+    }
+
+  
 
   var canvas = document.getElementById("game");
   var ctx = canvas.getContext("2d");
   var background = new Background(canvas);
   var player = new Player(canvas);
+  var things = new Things(canvas);
 
 
 
@@ -96,6 +138,9 @@ Player.prototype.move=function(){
 
       player.draw();
       player.move();
+
+      things.draw();
+      things.move();
       
 
 
